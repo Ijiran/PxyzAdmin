312ef0785d6a62f39a97b0e1db09789a83cf1c6d
Ijiran <15732749090@163.com>
2020-06-23 23:40:16 +0800

    IpController

--- src/main/java/top/pxyz/pxyzadmin/system/ip/controller/IPController.java
+++ src/main/java/top/pxyz/pxyzadmin/system/ip/controller/IPController.java
@@ -1,6 +1,7 @@
 package top.pxyz.pxyzadmin.system.ip.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * IPController
@@ -11,6 +12,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class IPController {
 
+    /**
+     * ip管理首页
+     * @return
+     */
+    @RequestMapping("index")
+    public String toIndex(){
+        return "admin/system/ip/index";
+    }
 
 
 }

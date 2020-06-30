a90fe01118f6af9f68cda43c9c383e043d173ade
Ijiran <15732749090@163.com>
2020-06-30 22:52:13 +0800

    oshi.js 请求参数赋值

--- src/main/resources/static/js/system/oshi/oshi.js
+++ src/main/resources/static/js/system/oshi/oshi.js
@@ -3,7 +3,20 @@
  * */
 layui.use(['jquery'], function () {
     var $ = layui.jquery;
-
-
-
+    $.ajax({
+        type: "get",
+        url: '/pxyzadmin/oshi/getOshiParam',
+        cache: false,
+        async: false,
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            if(data.result == 'success'){
+                //循环赋值即可
+                for (var key in data.data){
+                    $("span[name='"+key+"']").text(data.data[key]);
+                }
+            }
+        }
+    });
 });
